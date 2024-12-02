7dc52dd3ad96bc8892bbe509f0eef1c6cd201b0f
RomannaBidnyk <romanna.bidnyk@gmail.com>
2024-12-02 16:57:43 -0500

    answers week 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import AddTodoForm from "./AddTodoForm";
 import "./App.css";
 import TodoList from "./TodoList";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
   return (
     <>
       <div>
         <h1>ToDo List</h1>
-        <AddTodoForm />
+        <AddTodoForm onAddTodo={setNewTodo} />
+        <p>{newTodo}</p>
         <TodoList />
       </div>
     </>
---
+++ src/TodoListItem.jsx
@@ -0,0 +1,9 @@
+function TodoListItem({ item }) {
+  return (
+    <>
+      <li>{item.title}</li>
+    </>
+  );
+}
+
+export default TodoListItem;
