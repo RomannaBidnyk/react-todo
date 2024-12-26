657d61e72839d4adf6f06a2aaf014189d4119ea4
RomannaBidnyk <romanna.bidnyk@gmail.com>
2024-12-25 22:37:57 -0500

    Week 6: Hooks and Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTodoForm from "./AddTodoForm";
 import "./App.css";
 import TodoList from "./TodoList";
 
+function useSemiPersistentState(key, initialValue) {
+  const [state, setState] = useState(() => {
+    const savedState = localStorage.getItem(key);
+    return savedState ? JSON.parse(savedState) : initialValue;
+  });
+
+  useEffect(() => {
+    if (state.length > 0) {
+      localStorage.setItem(key, JSON.stringify(state));
+    }
+  }, [state, key]);
+
+  return [state, setState];
+}
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", []);
 
   const addTodo = (newTodo) => {
     setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
   };
 
   return (
-    <div>
+    <>
       <h1>ToDo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
