cf71626d5a985803a9fa6c43601e455974625904
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-02-24 19:33:03 -0500

    feat: added checkbox to mark items as completed

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,9 +1,21 @@
 import style from "./TodoListItem.module.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 function TodoListItem({ item: todo, onRemoveTodo }) {
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsCompleted((prev) => !prev);
+  };
+
   return (
-    <li className={style.ListItem}>
+    <li className={`${style.ListItem} ${isCompleted ? style.completed : ""}`}>
+      <input
+        type="checkbox"
+        checked={isCompleted}
+        onChange={handleCheckboxChange}
+      />
       <span>{todo.title}</span>
       <button
         className={style.removeButton}
